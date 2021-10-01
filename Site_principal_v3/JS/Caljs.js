b5352b5bfbe0e83f047548623395428470a14ef8
augustoCSP <augusto.pires@bandtec.com.br>
2021-10-01 12:53:33 -0300

    localStorage para validação de url do site com uso de if e else

--- Site_principal_v3/JS/Caljs.js
+++ Site_principal_v3/JS/Caljs.js
@@ -1,3 +1,12 @@
+// localStorage.setItem('email', 'teste@bandtec.com.br')
+// localStorage.setItem('passaword', 1234)
+// var email = localStorage.getItem('email')
+// var senha = localStorage.getItem('passaword')
+// if (email != true && senha != true){
+//     alert('Não conectado')
+//     window.location.href ='tela_cadastro.html'
+// }
+
 var linha_selecionada;
 var plano;
 var preco;
