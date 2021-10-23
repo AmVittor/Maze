1a470ea358ebeb9890526c6d1e2e6a51e7a2d9e8
Vitor <deadmilizer@gmail.com>
2021-10-23 17:44:04 -0300

    Re organizado as pastas, deletado arquivos que nao eram mais utilizados

--- Site_Maze/JS/Caljs.js
+++ Site_Maze/JS/Caljs.js
@@ -2,9 +2,9 @@
 // localStorage.setItem('passaword', 1234)
 var email = localStorage.getItem('email')
 var senha = localStorage.getItem('passaword')
-if (email != 'teste@bandtec.com.br' && senha != "1234"){
+if (email != 'teste@bandtec.com.br' && senha != "1234") {
     alert('Não conectado')
-    window.location.href ='tela_cadastro.html'
+    window.location.href = 'tela_cadastro.html'
 }
 
 var linha_selecionada;
@@ -19,30 +19,30 @@ function fnCalculo() {
     //Guardando em uma váriavel a quantidade de pessoas de acordo com a linha selecionada
     var qtd_pessoas = fnQtdPessoas(linha_selecionada);
     var visualizacoes_por_dia = qtd_pessoas;
-        //Cálculo da quantidade de pessoas por mês
-        var visualizacoes_mensais = (visualizacoes_por_dia * 30).toLocaleString();
-        //Resultado financeiro
-        var resultado_financeiro = (preco * qtd_pessoas * 0.01).toLocaleString();
-         //Mostrar o retorno financeiro e as visualizações
-         tempo.innerHTML = `Selecionando o plano <strong style="color: yellow">${plano}</strong>, na linha <strong style="color: yellow">${linha_selecionada}</strong>, seu anúncio terá aproximadamente: <br><br>
-         <strong id="views_dia" style="color: yellow">${visualizacoes_por_dia}</strong> visualizações por dia,<br>
-         <strong id="views_mes" style="color: yellow">${visualizacoes_mensais}</strong> visualizações por mês e,<br><br>
-         <strong id="resultado_financeiro" style="color: yellow">R$${resultado_financeiro}</strong> de retorno financeiro, aproximadamente.   
+    //Cálculo da quantidade de pessoas por mês
+    var visualizacoes_mensais = (visualizacoes_por_dia * 30).toLocaleString();
+    //Resultado financeiro
+    var resultado_financeiro = (preco * qtd_pessoas * 0.01).toLocaleString();
+    //Mostrar o retorno financeiro e as visualizações
+    tempo.innerHTML = `Selecionando o plano <strong style="color: white">${plano}</strong>, na linha <strong style="color: white">${linha_selecionada}</strong>, seu anúncio terá aproximadamente: <br><br>
+         <strong id="views_dia" style="color: white">${visualizacoes_por_dia}</strong> visualizações por dia,<br>
+         <strong id="views_mes" style="color: white">${visualizacoes_mensais}</strong> visualizações por mês e,<br><br>
+         <strong id="resultado_financeiro" style="color: white">R$${resultado_financeiro}</strong> de retorno financeiro, aproximadamente.   
          `;
 
-         function fnQtdPessoas(linha) {
-            if (linha == "1") {
-                return 489000;
-            } else if (linha == "2") {
-                return 275000;
-            } else if (linha == "3") {
-                return 603000;
-            } else if (linha == "4") {
-                return 350000;
-            } else if (linha == "5") {
-                return 87900;
-            } else if (linha == "15") {
-                return 43000;
-            }
+    function fnQtdPessoas(linha) {
+        if (linha == "1") {
+            return 489000;
+        } else if (linha == "2") {
+            return 275000;
+        } else if (linha == "3") {
+            return 603000;
+        } else if (linha == "4") {
+            return 350000;
+        } else if (linha == "5") {
+            return 87900;
+        } else if (linha == "15") {
+            return 43000;
         }
     }
+}
