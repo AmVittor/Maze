05ef71cc9f7e32d166de1e3cf086392e54f7cf40
diegosvg01 <diego.guimaraes@bandtec.com.br>
2021-10-28 23:44:41 -0300

    correção dos bugs dos cálculos exibidos pelo simulador

--- Site_Maze/JS/Caljs.js
+++ Site_Maze/JS/Caljs.js
@@ -33,19 +33,32 @@ var preco;
 function fnCalculo() {
     linha_selecionada = linha.value;
     plano = selecao.value;
-  
+    
     //Guardando em uma váriavel a quantidade de pessoas de acordo com a linha selecionada
     var qtd_pessoas = fnQtdPessoas(linha_selecionada);
     var visualizacoes_por_dia = qtd_pessoas;
     //Cálculo da quantidade de pessoas por mês
     var visualizacoes_mensais = (visualizacoes_por_dia * 30).toLocaleString();
+
+    var visualizacoes_semestrais = ((visualizacoes_por_dia * 30) * 6).toLocaleString();
+    
+    var visualizacoes_anuais = ((visualizacoes_por_dia * 30) * 12).toLocaleString();
+
     //Resultado financeiro
     // var resultado_financeiro = (preco * qtd_pessoas * 0.01).toLocaleString();
     //Mostrar o retorno financeiro e as visualizações
     tempo.innerHTML = `Selecionando o plano <strong style="color: white">${plano} dias</strong>, com fluxo <strong style="color: white">${linha_selecionada}</strong>, seu anúncio terá aproximadamente: <br><br>
-         <strong id="views_dia" style="color: white">${visualizacoes_por_dia}</strong> visualizações por dia,<br>
-         <strong id="views_mes" style="color: white">${visualizacoes_mensais}</strong> visualizações por mês <br><br>   
+         <span id="span_views_dia"><strong id="views_dia" style="color: white">${visualizacoes_por_dia}</strong> visualizações por dia,</span><br>
+         <span id="span_views_mes"><strong id="views_mes" style="color: white">${visualizacoes_mensais}</strong> visualizações por mês </span> <br><br>   
          `;
+
+        if(plano == "180") {
+            span_views_dia.innerHTML = `<strong id="views_dia" style="color: white">${visualizacoes_mensais}</strong> visualizações por mês,`
+            span_views_mes.innerHTML = `<strong id="views_mes" style="color: white">${visualizacoes_semestrais}</strong> visualizações por semestre`
+        } else if (plano == "365") {
+            span_views_dia.innerHTML = `<strong id="views_dia" style="color: white">${visualizacoes_semestrais}</strong> visualizações por semestre,`
+            span_views_mes.innerHTML = `<strong id="views_mes" style="color: white">${visualizacoes_anuais}</strong> visualizações por ano`
+        }
 
     function fnQtdPessoas(linha) {
         if (linha == "Baixo") {
@@ -54,6 +67,6 @@ function fnCalculo() {
             return 275000;
         } else if (linha == "Alto") {
             return 603000;
-        }
+        } 
     }
 }
