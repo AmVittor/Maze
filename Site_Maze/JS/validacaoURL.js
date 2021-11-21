24c210dcbbf97fb0c46336b286968cb3b72e48de
diegosvg01 <diego.guimaraes@bandtec.com.br>
2021-11-21 16:58:43 -0300

    integração do site com o banco de dados

--- Site_Maze/JS/validacaoURL.js
+++
@@ -1,8 +0,0 @@
-var email_local = localStorage.getItem('email_emp')
-var senha_local = localStorage.getItem('passaword_emp')
-
-
-if (email_local == ' ' && senha_local == ' ') {
-    alert('Não conectado')
-    window.location.href = 'cadastro.html'
-}
