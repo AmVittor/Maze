5de721a5b06f5b3372b5dec3dddea991dc5dd42c
aluno <aluno@aluno.com>
2021-11-26 16:24:36 -0300

    Alterada toda validação, login e cadastro funcionando 100%

--- SiteMaze/public/js/funcoes.js
+++ SiteMaze/public/js/funcoes.js
@@ -8,17 +8,15 @@ function validarSessao() {
     var h1LoginUsuario = document.getElementById("h1_login_usuario");
 
     if (email != null && nome != null) {
-        // window.alert(`Seja bem-vindo, ${nome}!`);
-        if (h1LoginUsuario != undefined) {
-            h1LoginUsuario.innerHTML = email;
-        }
-        b_usuario.innerHTML = nome;
+
+        em_login.innerHTML = nome;
 
         // finalizarAguardar();
     } else {
         window.location = "./index.html";
     }
 }
+//}
 
 function limparSessao() {
     // aguardar();
